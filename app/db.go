188e908c1c9d33062285ee6269ddea5771d1f330
kerbrek <kerbrek@gmail.com>
2022-04-30 12:11:07 +0200

    refactoring

--- app/db.go
+++ app/db.go
@@ -53,3 +53,61 @@ func dropTables() {
 		log.Fatal(err)
 	}
 }
+
+func saveTickers(tickers []TickerIn) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(
+		`INSERT INTO tickers (symbol, price_24h, volume_24h, last_trade_price)
+			VALUES ($1, $2, $3, $4) ON CONFLICT (symbol) DO UPDATE
+			SET price_24h = excluded.price_24h,
+				volume_24h = excluded.volume_24h,
+				last_trade_price = excluded.last_trade_price`,
+	)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, t := range tickers {
+		_, err := stmt.Exec(t.Symbol, t.Price24h, t.Volume24h, t.LastTradePrice)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readTickers() (map[string]TickerOut, error) {
+	rows, err := db.Query("SELECT symbol, price_24h, volume_24h, last_trade_price FROM tickers ORDER BY symbol")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	tickers := make(map[string]TickerOut)
+
+	for rows.Next() {
+		var symbol string
+		var t TickerOut
+		if err := rows.Scan(&symbol, &t.Price, &t.Volume, &t.LastTrade); err != nil {
+			return nil, err
+		}
+		tickers[symbol] = t
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tickers, nil
+}
--- app/main.go
+++ app/main.go
@@ -84,39 +84,6 @@ func downloadTickers(client *http.Client) ([]TickerIn, error) {
 	return tickers, nil
 }
 
-func saveTickers(tickers []TickerIn) error {
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	stmt, err := tx.Prepare(
-		`INSERT INTO tickers (symbol, price_24h, volume_24h, last_trade_price)
-			VALUES ($1, $2, $3, $4) ON CONFLICT (symbol) DO UPDATE
-			SET price_24h = excluded.price_24h,
-				volume_24h = excluded.volume_24h,
-				last_trade_price = excluded.last_trade_price`,
-	)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	for _, t := range tickers {
-		_, err := stmt.Exec(t.Symbol, t.Price24h, t.Volume24h, t.LastTradePrice)
-		if err != nil {
-			return err
-		}
-	}
-
-	if err := tx.Commit(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func updateTickers(client *http.Client) error {
 	tickers, err := downloadTickers(client)
 	if err != nil {
@@ -143,32 +110,12 @@ func updateTickersByInterval(interval time.Duration, client *http.Client) {
 }
 
 func getTickers(c *gin.Context) {
-	rows, err := db.Query("SELECT symbol, price_24h, volume_24h, last_trade_price FROM tickers ORDER BY symbol")
+	tickers, err := readTickers()
 	if err != nil {
 		c.AbortWithStatus(http.StatusInternalServerError)
 		log.Print(err)
 		return
 	}
-	defer rows.Close()
 
-	tickersMap := make(map[string]TickerOut)
-
-	for rows.Next() {
-		var symbol string
-		var t TickerOut
-		if err := rows.Scan(&symbol, &t.Price, &t.Volume, &t.LastTrade); err != nil {
-			c.AbortWithStatus(http.StatusInternalServerError)
-			log.Print(err)
-			return
-		}
-		tickersMap[symbol] = t
-	}
-
-	if err := rows.Err(); err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		log.Print(err)
-		return
-	}
-
-	c.IndentedJSON(http.StatusOK, tickersMap)
+	c.IndentedJSON(http.StatusOK, tickers)
 }
